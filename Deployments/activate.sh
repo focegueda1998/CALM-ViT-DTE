97f0272f87afd067b2f46e21dc28a1183320e64e
Francisco Ocegueda Jr. <focegueda@csu.fullerton.edu>
2025-03-03 01:53:26 -0800

    Added timer to call deactivation script

--- Deployments/activate.sh
+++ Deployments/activate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Deploying Spark Cluster..."
-kubectl apply -f service.yaml -f pvc.yaml -f slave01-pod.yaml -f slave02-pod.yaml -f slave03-pod.yaml -f master-pod.yaml
+kubectl apply -f ingress.yaml -f service.yaml -f pvc.yaml -f slave01-pod.yaml -f slave02-pod.yaml -f slave03-pod.yaml -f master-pod.yaml
 
 while kubectl get pods | grep -q -E "Pending|Init:[0-9]+/[0-9]+|ContainerCreating|Succeeded|Failed|Unknown|CrashLoopBackOff|Error";
 do
@@ -12,3 +12,7 @@ done
 echo "Copying Codebase to shared volume..."
 kubectl cp ../Codebase master:/config
 echo "Codebase copied to shared volume. Deployment complete."
+echo "Waiting 20700 seconds until deactivating..."
+sleep 20700
+echo "Executing deactivation script..."
+bash ./deactivate.sh
