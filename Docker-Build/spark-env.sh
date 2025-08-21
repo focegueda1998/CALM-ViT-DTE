b8573e688f2691a1100277bbc8bb41eabcb40648
Francisco Ocegueda Jr. <focegueda@csu.fullerton.edu>
2025-08-21 00:34:25 -0700

    Updated dependencies for Spark Torch Container

--- Docker-Build/spark-env.sh
+++ Docker-Build/spark-env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-HADOOP_CONF_DIR=/usr/local/hadoop-3.4.0/etc/hadoop
-YARN_CONF_DIR=/usr/local/hadoop-3.4.0/etc/hadoop
+HADOOP_CONF_DIR=/usr/local/hadoop-3.4.1/etc/hadoop
+YARN_CONF_DIR=/usr/local/hadoop-3.4.1/etc/hadoop
 export SPARK_MASTER_HOST=$(hostname)
 export SPARK_MASTER_PORT=7077
 export SPARK_RAPIDS_DIR=/opt/sparkRapidsPlugin
-export SPARK_RAPIDS_PLUGIN_JAR=${SPARK_RAPIDS_DIR}/rapids-4-spark_2.13-24.10.1.jar
+export SPARK_RAPIDS_PLUGIN_JAR=${SPARK_RAPIDS_DIR}/rapids-4-spark_2.13-25.08.0.jar
 SPARK_MASTER_WEBUI_PORT=8080
 SPARK_WORKER_PORT=7078
 SPARK_WORKER_WEBUI_PORT=8081
